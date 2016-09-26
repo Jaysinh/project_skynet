a112e5c648619a20f03473c1086de5672a504ea8
Jaysinh Sagar <jaysinh.sagar@gmail.com>
2016-09-26 14:25:18 +0400

    added boost_server

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include "boost_server/server.hpp"
+#include <wiringPi.h>
 
 using namespace std;
 
 int main()
 {
-	cout<<"hello world!"<<endl;
+	try
+	{
+	char const *ip="192.168.0.175";
+	char const *port="5555";
+	char const *home=".";
+	std::cout<<"initialising server at "<<ip<<":"<<port<<std::endl;
+	http::server::server s(ip, port, home);
+
+	s.run();
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << "exception: " << e.what() << "\n";
+	}
 	return 0;
 }
